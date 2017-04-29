f1d174736a750f0bb020e0197a2937c9a3c6bc13
purple-magic <markop.018@gmail.com>
2017-04-30 00:17:24 +0200

    REST SetCustomerDetails method correction

--- SOAP-REST_Server/src/generated/com/purplemagic/ws/generated/Customer.java
+++ SOAP-REST_Server/src/generated/com/purplemagic/ws/generated/Customer.java
@@ -2,9 +2,16 @@
 package com.purplemagic.ws.generated;
 
 import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -39,8 +46,13 @@ import javax.xml.bind.annotation.XmlType;
     "email",
     "phoneNumber"
 })
+@Entity
+@Table
+@XmlRootElement(name = "Customer")
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
     protected BigInteger customerId;
     @XmlElement(required = true)
     protected String firstName;
